142e0d9207504aa18343c4806e381d72f54bbffb
Alessio Morteo <alessiomorteo@users.noreply.github.com>
2023-03-02 22:10:43 +0000

    Special card class progressed a small amount

--- src/monopoly_special_card.py
+++ src/monopoly_special_card.py
@@ -1,5 +1,18 @@
 
 class SpecialCard:
     def __init__(self, name:str, description: str)->None:
+        """Initialise an instance of the SpecialCard to
+        represent a card.
+
+        Args:
+            name (str): The name of the card
+            description (str): The full description of the card.
+        """
         self.name = name
         self.description = description
+
+    def execute_card_action(self)->None:
+        """Function for executing the action defined by the card.
+        Includes actions such as financial and positional.
+        """
+        pass
