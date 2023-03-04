cd040e0659f38472a27fb418ea83de3e7263353d
Alessio Morteo <alessiomorteo@users.noreply.github.com>
2023-03-04 18:32:13 +0000

    Init py setup

--- src/init.py
+++ src/init.py
@@ -1,2 +1,15 @@
-if True:
-    pass
+from .monopoly_board import MonopolyBoard
+from .monopoly_card_deck import CardDeck
+from .monopoly_game import MonopolyGame
+from .monopoly_player import MonopolyPlayer
+from .monopoly_property import MonopolyProperty
+from .monopoly_special_card import SpecialCard
+
+__all__ = [
+    "MonopolyBoard",
+    "CarDeck",
+    "MonopolyGame",
+    "MonopolyPlayer",
+    "MonopolyProperty",
+    "SpecialCard"
+]
